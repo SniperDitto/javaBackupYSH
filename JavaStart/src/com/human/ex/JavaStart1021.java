b4900665bddb0b37fd50db2a8746726db0fcefc5
123 <123@123-PC.local>
2020-10-21 12:20:11 +0900

    qwer출력테스트

--- JavaStart/src/com/human/ex/JavaStart1021.java
+++ JavaStart/src/com/human/ex/JavaStart1021.java
@@ -3,8 +3,17 @@ package com.human.ex;
 public class JavaStart1021 {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println("qwer");
+		/*
+		 * 자료구조 - 정렬 배열, 리스트, 스택, 큐, 트리, (그래프), 네트워크, 채팅(쓰레드) 
+		 * git에 배운 것들 정리 
+		 * html, css, javascript, jquery -웹 
+		 * +java, jsp, spring, spring boot
+		 * bootstrap, vue, angular, react -GUI
+		 * oracle, ms sql, sqlite, mysql, mongo(객체지향db) -db
+		 * c#, asp .net / c, php -서버
+		 * nodejs
+		 */
 	}
 
 }
