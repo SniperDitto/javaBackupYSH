ce956920dccf38e0ac576ee1544d3b56fac74e76
123 <123@123-PC.local>
2020-10-22 14:06:47 +0900

    차ABC

--- JavaStart/src/com/human/ex/JavaStart1022.java
+++ JavaStart/src/com/human/ex/JavaStart1022.java
@@ -8,15 +8,18 @@ public class JavaStart1022 {
 
 	public static void main(String[] args) {
 		java.util.Scanner scanner =new java.util.Scanner(System.in);
-		Car car = new Car();
-		Car.Wheel wheelA = car.new Wheel();
+		Car carA = new Car();
+		Car.Wheel wheelA = carA.new Wheel();
+		Car carB = new Car();
+		Car.Wheel wheelB = carB.new Wheel();
+		Car carC = new Car();
+		Car.Wheel wheelC = carC.new Wheel();
 
-		for(int i=1;i<=3;i++) {
-			System.out.printf("차%d의 정보 입력%n",i);
+			System.out.printf("차A의 정보 입력%n");
 			System.out.print("색깔 : ");
-			car.color = scanner.nextLine();
+			carA.color = scanner.nextLine();
 			System.out.print("바퀴 종류: ");
-			car.wheel = scanner.nextLine();
+			carA.wheel = scanner.nextLine();
 			System.out.print("바퀴 제조사 : ");
 			wheelA.companyWheel = scanner.nextLine();
 			System.out.print("바퀴 가격: ");
@@ -24,17 +27,58 @@ public class JavaStart1022 {
 			System.out.print("바퀴 주행거리: ");
 			wheelA.distance = Integer.parseInt(scanner.nextLine());
 			System.out.print("차 제조사 : ");
-			car.companyCar = scanner.nextLine();
+			carA.companyCar = scanner.nextLine();
 			System.out.print("최고속도 : ");
-			car.maxSpeed = Integer.parseInt(scanner.nextLine());
+			carA.maxSpeed = Integer.parseInt(scanner.nextLine());
 			System.out.print("차종 : ");
-			car.carType = scanner.nextLine();
-			
-			System.out.printf("차 %s%n색깔 %s%n바퀴종류 %s%n바퀴제조사 %s%n가격 %d%n주행거리 %d%n차제조사 %s%n최고속도 %d%n차종 %s%n"
-					,i,car.color,car.wheel,wheelA.companyWheel,wheelA.cost,wheelA.distance
-					,car.companyCar,car.maxSpeed,car.carType);
+			carA.carType = scanner.nextLine();
+			System.out.printf("차 A%n색깔 %s%n바퀴종류 %s%n바퀴제조사 %s%n가격 %d%n주행거리 %d%n차제조사 %s%n최고속도 %d%n차종 %s%n"
+					,carA.color,carA.wheel,wheelA.companyWheel,wheelA.cost,wheelA.distance
+					,carA.companyCar,carA.maxSpeed,carA.carType);
 
-		}
+			
+			System.out.printf("차B의 정보 입력%n");
+			System.out.print("색깔 : ");
+			carB.color = scanner.nextLine();
+			System.out.print("바퀴 종류: ");
+			carB.wheel = scanner.nextLine();
+			System.out.print("바퀴 제조사 : ");
+			wheelB.companyWheel = scanner.nextLine();
+			System.out.print("바퀴 가격: ");
+			wheelB.cost = Integer.parseInt(scanner.nextLine());
+			System.out.print("바퀴 주행거리: ");
+			wheelB.distance = Integer.parseInt(scanner.nextLine());
+			System.out.print("차 제조사 : ");
+			carB.companyCar = scanner.nextLine();
+			System.out.print("최고속도 : ");
+			carB.maxSpeed = Integer.parseInt(scanner.nextLine());
+			System.out.print("차종 : ");
+			carB.carType = scanner.nextLine();
+			System.out.printf("차 B%n색깔 %s%n바퀴종류 %s%n바퀴제조사 %s%n가격 %d%n주행거리 %d%n차제조사 %s%n최고속도 %d%n차종 %s%n"
+					,carB.color,carB.wheel,wheelB.companyWheel,wheelB.cost,wheelB.distance
+					,carB.companyCar,carB.maxSpeed,carB.carType);
+			
+			
+			System.out.printf("차C의 정보 입력%n");
+			System.out.print("색깔 : ");
+			carC.color = scanner.nextLine();
+			System.out.print("바퀴 종류: ");
+			carC.wheel = scanner.nextLine();
+			System.out.print("바퀴 제조사 : ");
+			wheelC.companyWheel = scanner.nextLine();
+			System.out.print("바퀴 가격: ");
+			wheelC.cost = Integer.parseInt(scanner.nextLine());
+			System.out.print("바퀴 주행거리: ");
+			wheelC.distance = Integer.parseInt(scanner.nextLine());
+			System.out.print("차 제조사 : ");
+			carC.companyCar = scanner.nextLine();
+			System.out.print("최고속도 : ");
+			carC.maxSpeed = Integer.parseInt(scanner.nextLine());
+			System.out.print("차종 : ");
+			carC.carType = scanner.nextLine();
+			System.out.printf("차 C%n색깔 %s%n바퀴종류 %s%n바퀴제조사 %s%n가격 %d%n주행거리 %d%n차제조사 %s%n최고속도 %d%n차종 %s%n"
+					,carC.color,carC.wheel,wheelC.companyWheel,wheelC.cost,wheelC.distance
+					,carC.companyCar,carC.maxSpeed,carC.carType);
 
 	}
 
